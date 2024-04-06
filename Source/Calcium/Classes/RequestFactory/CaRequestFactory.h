8d7db010222ef0887c096f478c7f59c4434c74de
bag-xml <gaming.mali357@gmail.com>
2024-04-06 22:21:04 +0200

    request factory

---
+++ Source/Calcium/Classes/RequestFactory/CaRequestFactory.h
@@ -0,0 +1,13 @@
+//
+//  CaRequestFactory.h
+//  Calcium
+//
+//  Created by bag.xml on 06/04/24.
+//  Copyright (c) 2024 Mali 357. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CaRequestFactory : NSObject
+
+@end
---
+++ Source/Calcium/Classes/RequestFactory/CaRequestFactory.m
@@ -0,0 +1,13 @@
+//
+//  CaRequestFactory.m
+//  Calcium
+//
+//  Created by bag.xml on 06/04/24.
+//  Copyright (c) 2024 Mali 357. All rights reserved.
+//
+
+#import "CaRequestFactory.h"
+
+@implementation CaRequestFactory
+
+@end
